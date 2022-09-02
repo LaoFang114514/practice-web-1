36bbc13aa031e6519429d79a202f41293070d8b4
老方-LaoFang <107829543+LaoFang114514@users.noreply.github.com>
2022-09-02 23:37:23 +0800

    Add files via upload

---
+++ css/start.css
@@ -0,0 +1,63 @@
+
+.anniu {
+    background-color: #17c9ffc5; 
+    border: none;
+    color: white;
+    padding: 14px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 5px 2px;
+    -webkit-transition-duration: 0.4s;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    opacity:0.7;
+}
+.anniu:hover {
+    background-color: #ffffff;
+    color: #17c9ffc5;
+    opacity:1.0;
+}
+.box{
+    width:80%;
+    margin:50px 80px;
+    background-color:#e4f7ff;
+    border:1px solid #d1faff;
+    opacity:0.6;
+    margin: auto;
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    border-radius: 20px;
+}
+.boxbox{
+    width: 80%;
+    margin: auto;
+    text-align: center;
+}
+.di{    
+    background-color: #17c9ffc5; 
+    border: none;
+    color: white;
+    padding: 7px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 2px;
+    margin: 3px 2px;
+    -webkit-transition-duration: 0.4s;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    opacity:0.7;
+    border-radius: 16px;
+    margin: auto;
+    text-align: center;
+}
+.logo{
+    display: block;
+    margin: 0 auto;
+}
+.boxaj{width: 90%; margin: auto;text-align: center;}
+p.Bttf{font-family:"font/",Times,serif;}
+.yuanjiao {border-radius: 18px;}
+.zhong{padding: 15% 0;}
